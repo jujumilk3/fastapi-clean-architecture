ab6701c9059f632683084edeb1cbf97092d2643e
jujumilk3 <jujumilk3@gmail.com>
2022-05-28 22:42:48 +0900

    FEATURE - apply singleton at app

--- app/main.py
+++ app/main.py
@@ -6,30 +6,39 @@ from app.core.config import settings
 from app.core.container import Container
 
 
-app = FastAPI(
-    title=settings.PROJECT_NAME,
-    openapi_url=f'{settings.API_V1_STR}/openapi.json',
-    version='0.0.1'
-)
+class AppCreator:
+    def __init__(self):
+        # set app default
+        self.app = FastAPI(
+            title=settings.PROJECT_NAME,
+            openapi_url=f'{settings.API_V1_STR}/openapi.json',
+            version='0.0.1'
+        )
 
-container = Container()
-db = container.db()
-db.create_database()
+        # set db and container
+        self.container = Container()
+        self.db = self.container.db()
+        self.db.create_database()
 
-# Set all CORS enabled origins
-if settings.BACKEND_CORS_ORIGINS:
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
-        allow_credentials=True,
-        allow_methods=['*'],
-        allow_headers=['*'],
-    )
+        # set cors
+        if settings.BACKEND_CORS_ORIGINS:
+            self.app.add_middleware(
+                CORSMiddleware,
+                allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+                allow_credentials=True,
+                allow_methods=['*'],
+                allow_headers=['*'],
+            )
+
+        # set routes
+        @self.app.get('/')
+        def read_root():
+            return "service is working"
+
+        self.app.include_router(routers, prefix=settings.API_V1_STR)
 
 
-@app.get('/')
-def root():
-    return "service is working"
-
-
-app.include_router(routers, prefix=settings.API_V1_STR)
+app_creator = AppCreator()
+app = app_creator.app
+db = app_creator.db
+container = app_creator.container
