24705ab0c7d4e74cc6088172570dcce5699432ad
jujumilk3 <jujumilk3@gmail.com>
2023-03-07 02:43:16 +0900

    restore sqlmodel

--- app/model/base_model.py
+++ app/model/base_model.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 import re
 import pytz
-from sqlalchemy import Column, DateTime, func, Integer, String
+from sqlalchemy import func
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
-from sqlalchemy.ext.declarative import declarative_base
 
 
 def resolve_table_name(name: str) -> str:
@@ -16,12 +15,11 @@ class Base(DeclarativeBase):
     def __tablename__(self):
         return resolve_table_name(self.__name__)
 
-    id: Mapped[int] = mapped_column(primary_key=True, index=True, nullable=False)
+    id: int = mapped_column(primary_key=True, index=True, nullable=False)
     created_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), nullable=False, server_default=func.now(), default=datetime.now(pytz.utc)
+        nullable=False, server_default=func.now(), default=datetime.now(pytz.utc)
     )
     updated_at: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True),
         nullable=False,
         server_default=func.now(),
         default=datetime.now(pytz.utc),
@@ -38,28 +36,18 @@ class User(Base):
     is_active: Mapped[bool] = mapped_column(default=False, nullable=False)
     is_superuser: Mapped[bool] = mapped_column(default=False, nullable=False)
 
-#
-# Base2 = declarative_base()
-#
-# class MyModel(Base2):
-#     __tablename__ = 'mymodel'
-#
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String)
-#     created_at = Column(DateTime(timezone=True), default=func.now())
-
 
 if __name__ == "__main__":
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
 
     engine = create_engine("sqlite:///:memory:", echo=True)
-    #postgres engine
+    # postgres engine
     engine = create_engine("postgresql://damon:@localhost:5432/fca", echo=True)
     # Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    # session.add(User(email="test3@test.com", password="test3", user_token="test3"))
+    # session.add(User(email="test4@test.com", password="test4", user_token="test4"))
     # session.commit()
     # print(session.query(User).all())
     # user = session.query(User).first()
@@ -76,6 +64,9 @@ if __name__ == "__main__":
     for user in users:
         print(user.created_at.astimezone(pytz.utc))
         print(user.updated_at)
+        print(type(user.name))
+        print(user.name.__dict__)
+        # Mapped is not so good. it can't type tracking.
     # create tables
     # Base2.metadata.create_all(engine)
     # session.add(MyModel(name="test"))
@@ -86,5 +77,3 @@ if __name__ == "__main__":
     # print(mymodel.created_at)
     # print(mymodel.created_at.astimezone())
     # print(mymodel.created_at.astimezone(pytz.utc))
-
-
--- app/model/post.py
+++ app/model/post.py
@@ -1,11 +1,11 @@
 from app.model.base_model import Base
 from sqlalchemy import Column, String, Boolean
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
 
 
 class Post(Base):
-    user_token: str = Column(String, nullable=False, unique=True)
+    user_token: Mapped[str] = mapped_column(String, nullable=False, unique=True)
 
-    title: str = Column(String, default="", nullable=False)
-    content: str = Column(String, default="", nullable=False)
-    is_published: bool = Column(Boolean, default=False, nullable=False)
+    title: Mapped[str] = mapped_column(String, default="", nullable=False)
+    content: Mapped[str] = mapped_column(String, default="", nullable=False)
+    is_published: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
